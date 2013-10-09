fd99415ffa45a929a6b8cc0a3ea186efe79053b9
Richard Stoutner <richard@mashica.org>
2013-10-09 13:16:38 -0500

    Added Ladino Pedido

---
+++ ladino.php
@@ -0,0 +1,110 @@
+<html>
+<head>
+	<title>Pedido Mashica Sport // 2013.10.09</title>
+<style type="text/css">
+body {	
+	padding-top: 20px;
+}
+h2 {
+	font:24px/24px Verdana, Arial, Helvetica, sans-serif;
+}
+h4 {
+	}
+td {
+	font:12px/15px Verdana, Arial, Helvetica, sans-serif;
+	vertical-align: top;
+	padding: 50px 5px;
+}
+.cantidad {
+	color:gray;
+}
+.total {
+	color:black;
+	font-weight: bold;
+}
+.subtitulo {
+	font:12px/15px Verdana, Arial, Helvetica, sans-serif;
+
+}
+</style>
+</head>
+<body>
+	<h2>Pedido Mashica Sport // 2013.10.09<br><span class="subtitulo">Ladino Ropa Ciclista</span></h2>
+	<h4></h4>
+	<table>
+		<tr>
+			<td>Jersey y Short<br><a href="ladino/jersey.short.ai">ai</a> | <a href="ladino/jersey.short.pdf">pdf</a>
+				<p># Jersey<br>
+					XS:   <span class="cantidad">1</span><br>
+					S:    <span class="cantidad">5</span><br>
+					M:    <span class="cantidad">9</span><br>
+					L:    <span class="cantidad">8</span><br>
+					XL:   <span class="cantidad">2</span><br>
+					XXL:  <span class="cantidad">1</span><br>
+					XXXL: <span class="cantidad">0</span><br>
+					Total: <span class="total">26</span></p>
+				<p># short<br>
+					XS:   <span class="cantidad">1</span><br>
+					S:    <span class="cantidad">5</span><br>
+					M:    <span class="cantidad">8</span><br>
+					L:    <span class="cantidad">9</span><br>
+					XL:   <span class="cantidad">3</span><br>
+					XXL:  <span class="cantidad">0</span><br>
+					XXXL: <span class="cantidad">0</span><br>
+					Total: <span class="total">26</span></p></td>
+			<td><img src="ladino/jersey.short.png"></td>
+		</tr>
+		<tr>
+			<td>Chaleco<br><a href="ladino/chaleco.pdf">pdf</a>
+				<p>#<br>
+					XS:   <span class="cantidad">0</span><br>
+					S:    <span class="cantidad">2</span><br>
+					M:    <span class="cantidad">5</span><br>
+					L:    <span class="cantidad">6</span><br>
+					XL:   <span class="cantidad">1</span><br>
+					XXL:  <span class="cantidad">1</span><br>
+					XXXL: <span class="cantidad">0</span><br>
+					Total: <span class="total">15</span></p></td>
+			<td><img src="ladino/chaleco.png"></td>
+		</tr>
+		<tr>
+			<td>Mangas<br><a href="ladino/mangas.pdf">pdf</a>
+				<p>#<br>
+					XS:   <span class="cantidad"></span><br>
+					S:    <span class="cantidad">2</span><br>
+					M:    <span class="cantidad">5</span><br>
+					L:    <span class="cantidad">5</span><br>
+					XL:   <span class="cantidad">1</span><br>
+					Total: <span class="total">13</span></p></td>
+			<td><img src="ladino/mangas.png"></td>
+		</tr>
+		<tr>
+			<td>Pierneras<br><a href="ladino/pierneras.pdf">pdf</a>
+				<p>#<br>
+					XS:   <span class="cantidad"></span><br>
+					S:    <span class="cantidad">1</span><br>
+					M:    <span class="cantidad">5</span><br>
+					L:    <span class="cantidad">5</span><br>
+					XL:   <span class="cantidad">2</span><br>
+					Total: <span class="total">13</span></p></td>
+			<td><img src="ladino/pierneras.png"></td>
+		</tr>
+		<tr>
+			<td>Guantes<br><a href="ladino/guantes.pdf">pdf</a>
+				<p>#<br>
+					S:    <span class="cantidad">2</span><br>
+					M:    <span class="cantidad">4</span><br>
+					L:    <span class="cantidad">6</span><br>
+					Total: <span class="total">12</span></p></td>
+			<td><img src="ladino/guantes.png"></td>
+		</tr>
+		<tr>
+			<td>Calcetas<br><a href="ladino/calcetas.pdf">pdf</a>
+				<p>#<br>
+					Total: <span class="total">30+?</span></p></td>
+			<td><img src="ladino/calcetas.png"></td>
+		</tr>
+ 	</table>
+
+</body>
+</html>
